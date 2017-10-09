1c45440a9096a4dfd7b6fa011caf9d0fa48a90a5
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-10-09 17:04:10 +0200

    reorder props
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- spec_test.go
+++ spec_test.go
@@ -54,10 +54,10 @@ func TestSpecYamlEncoding(t *testing.T) {
 					AltNames:       "kubernetes,kubernetes.default",
 					ClusterIPRange: "172.31.0.0/24",
 					Domain:         "api.giantswarm.io",
+					EncryptionKey:  "AWScv3423DEwcd",
 					IP:             net.ParseIP("172.31.0.1"),
 					InsecurePort:   8080,
 					SecurePort:     443,
-					EncryptionKey:  "AWScv3423DEwcd",
 				},
 				CloudProvider: "aws",
 				DNS: clustertprkubernetes.DNS{
